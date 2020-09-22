618d8552a122dc913f88c3182590473235440462
MaxC <MaxC@users.noreply.github.com>
2020-09-22 12:21:37 +0800

    picture process
    The image compression process is realized

--- NCM_Downloader.py
+++ NCM_Downloader.py
@@ -4,12 +4,16 @@ import re
 import urllib.request
 import os
 import sys
+import pngquant
 from pydub import AudioSegment
+from PIL import Image
+from os.path import join, getsize
 
 #Handling the problem of Windows reserved words in file names
-intab = "?*/\|.:><\""
-outtab = "          "
+intab = "?*/\\|.:><\"\'"
+outtab = "           "
 trantab = str.maketrans(intab, outtab)
+Error_List = "\n----------------------------------------------------\n"
 
 If_End = False
 while not If_End:
@@ -32,42 +36,65 @@ while not If_End:
     #Make sure the quality of music（standard by default
     Music_Quality = input("Please enter the quality of the song you wish to download: (standard/higher/exhigh/lossless)\n")
     Music_ID = int(input("Please enter the initial number of the music:\n"))
-    Check_Process = input("Do you want to transcode audio files, slice them, etc.    Y/N\n")
+    Check_Process = input("Do you want to transcode audio files, slice them and compress the images?    Y/N\n")
     for music in musiclist:
         music.name = music.name.translate(trantab)
-        music.download(Download_Addrass, Music_Quality)
-        File_Names = os.listdir(Download_Addrass)
-        path = Download_Addrass
-        for Name in File_Names:
-            for C in Name:
-                if C == '-':
-                    os.rename(os.path.join(Download_Addrass, Name), os.path.join(Download_Addrass, str(Music_ID).rjust(3,'0')+Name[Name.rindex('.'):]))
-                    break
-        urllib.request.urlretrieve(music.picUrl , filename=Download_Addrass+'/'+str(Music_ID).rjust(3,'0')+'.png')
-        Txt_File = open(r""+Download_Addrass + "/" + str(Music_ID).rjust(3,'0') + ".txt", mode='w', encoding="utf-8")
-        Txt_File.write("Music Name: "+music.name+"\n"+"Artist Name: ")
-        for artist in music.artist:
-            Txt_File.write(artist + " ")
-        Txt_File.write("\nAlbum Name: " + music.album + "\nLyrics:\n")
-        for lyrics in music.getLyrics():
-            if not lyrics is None:
-                Txt_File.write(lyrics)
-        Txt_File.write("Hot Comments:\n")
-        for comments in music.getHotComments():
-            if not comments is None:
-                Txt_File.write(comments['content']+"\n")
-        Txt_File.close()
-        print("NO."+str(Music_ID).rjust(3,'0')+" "+ music.name +" download successful!")
-        if Check_Process == "Y" or Check_Process == "y":
-            #Process audio files
-            if Name[Name.rindex('.')+1:] != "mp3":
-                Music_Process = AudioSegment.from_file(r""+Download_Addrass + "/" + str(Music_ID).rjust(3,'0')+Name[Name.rindex('.'):], Name[Name.rindex('.')+1:])
+        if not music.type is None:
+            try:
+                music.download(r""+Download_Addrass, Music_Quality)
+            except:
+                Error_List += "ID." + str(Music_ID) + "\n"
+                print("###Music ID " + str(music.id) + " has error, pass!")
+                continue
+        else:
+            Error_List += "ID." + str(music.id) + "\n"
+            print("###Music ID " + str(music.id) +" has error, pass!")
+            continue
+        try:
+            File_Names = os.listdir(Download_Addrass)
+            path = Download_Addrass
+            for Music_Name in File_Names:
+                for C in Music_Name:
+                    if C == '-':
+                        os.rename(os.path.join(Download_Addrass, Music_Name), os.path.join(Download_Addrass, str(Music_ID).rjust(3, '0') + '.'+music.type))
+                        break
+            print("NO." + str(Music_ID).rjust(3, '0') + " " + music.name + " download successful!")
+            if Check_Process == "Y" or Check_Process == "y":
+                #Process audio files
+                Music_Process = AudioSegment.from_file(r"" + Download_Addrass + "/" + str(Music_ID).rjust(3,'0') + '.'+music.type, music.type)
                 one_minute = 60 * 1000
                 Music_Save = Music_Process[:one_minute]
                 Music_Save.export(r""+Download_Addrass + "/" +str(Music_ID).rjust(3,'0')+".mp3",format ="mp3")
-                os.remove(os.path.join(Download_Addrass, str(Music_ID).rjust(3,'0')+Name[Name.rindex('.'):]))
-                print("NO." + str(Music_ID).rjust(3, '0') + " processing completed!")
+                if music.type != "mp3":
+                    os.remove(os.path.join(Download_Addrass, str(Music_ID).rjust(3,'0') + '.'+music.type))
+            Img_Name = Download_Addrass+'/'+str(Music_ID).rjust(3,'0')+'.png'
+            urllib.request.urlretrieve(music.picUrl , filename=Img_Name)
+            #pngquant.quant_image(Img_Name)
+            while getsize(Img_Name) > 1000000:
+                Img_Re = Image.open(Img_Name)
+                Img_Re = Img_Re.resize((int(Img_Re.size[0]/2),int(Img_Re.size[1]/2)))
+                Img_Re.save(Img_Name)
+            print("NO." + str(Music_ID).rjust(3, '0') + " processing completed!")
+            Txt_File = open(r""+Download_Addrass + "/" + str(Music_ID).rjust(3,'0') + ".txt", mode='w', encoding="utf-8")
+            Txt_File.write("Music Name: "+music.name+"\n\n"+"Artist Name: ")
+            for artist in music.artist:
+                Txt_File.write(artist + " ")
+            Txt_File.write("\n\nAlbum Name: " + music.album + "\n\nLyrics:\n")
+            for lyrics in music.getLyrics():
+                if not lyrics is None:
+                    Txt_File.write(lyrics)
+            Txt_File.write("\nHot Comments:\n")
+            for comments in music.getHotComments():
+                if not comments is None:
+                    Txt_File.write(comments['content']+"\n------------------------------\n")
+            Txt_File.close()
+            print("NO."+str(Music_ID).rjust(3,'0')+" .png and .txt file generated successful!")
+        except:
+            Error_List += "ID." + str(Music_ID) + "\n"
+            print("###Music ID " + str(music.id) + " has error, pass!")
+            continue
         Music_ID+=1
+    print(Error_List+"has errors!\nThe last ID is "+str(Music_ID-1)+"\n----------------------------------------------------")
     print("Do you want to continue running this program?    Y/N")
     Check_End = input()
     if Check_End == "n" or Check_End == "N":
--- README.md
+++ README.md
@@ -3,29 +3,35 @@
 ***
 * ___This program is based on the python package called [cloudmusic], you can visit the github home page of the package via the URL. Special thanks to the contributors of the package.___
 * ___This program also uses the [pydub] package, which you also need to install ahead of time. In particular, in order to use some of the features in the pydub package, you need to install [ffmpeg] or [libav] in your environment.___
+* ___In order to achieve the image compression function, we uses [pngquant] and [pillow]. So you will need to download the pngquant library and install the python wrapper and the pillow package.___
 
->###Functions realized in this program
+###Functions realized in this program
 
 > * According to the __ID__ of Netease Cloud Music playlist, automatically download all songs in the  playlist to the specified local folder
 > * Download album cover images of songs to the same directory
 > * Automatically creates txt files to save the song's name, authors, lyrics, hot comments and other information
 > * Automatically renames all files in sequence based on the initial number specified by the user
 > * Automatically convert audio files to __.mp3__ format and slice the first minute of the audio
+> * Automatically compress images larger than 1MB
 
->###How to use this program
+###How to use this program
 
 > * Configure your Python environment
+> * install [pngquant] on your OS
 > * install [ffmpeg] or [libav] on your OS
+> * install [pillow] according to the [document of pillow]
+> * install the [python wrapper of pngquant] using the command `pip install pngquant`
 > * install [pydub] package using the command `pip install pydub`
 > *  Install the [cloudmusic] package using the command `pip install cloudmusic`
 > *  Run the ___NCM_Downloader.py___ file
 > *  Follow the input prompt
 
->###Tips 
+###Tips 
 
 > * You can get the ID of the playlist from the URL of the Netease Cloud Music playlist
 > For example, in URL https://music.163.com/#/playlist?Id=4895239160, ___4895239160___ is the id of the playlist
 > * Try to make sure that the folder which you want to save musics is empty before you run the program, otherwise the program may report an error due to duplicate naming
+> * Some songs may fail to download, and the program will automatically skip and display an error list at the end of the run
 > *  This program is only tested in a Windows environment, and it is uncertain whether it will run successfully in other OS
 
 
@@ -34,3 +40,7 @@
 [pydub]: https://github.com/jiaaro/pydub
 [ffmpeg]: http://www.ffmpeg.org/
 [libav]: https://libav.org/
+[pngquant]: https://pngquant.org/
+[python wrapper of pngquant]: https://pypi.org/project/pngquant/
+[pillow]: https://python-pillow.org/
+[document of pillow]: https://pillow.readthedocs.io/en/latest/installation.html
